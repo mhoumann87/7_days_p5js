172b6df489f39e70d54587009fa21ea17a6c21f0
Michael Houmann <mhoumann87@gmail.com>
2022-06-10 14:18:54 +0200

    Start project

---
+++ 3_roses/ROSES.md
@@ -0,0 +1,5 @@
+# Mathematical Rose Patterns
+
+Based on an [article on Wikipedia](<https://en.wikipedia.org/wiki/Rose_(mathematics)>).
+
+Inspired by [the Coding Train Coding Challenge #55](https://www.youtube.com/watch?v=f5QBExMNB1I&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=72)
---
+++ 3_roses/sketch.js
@@ -0,0 +1,20 @@
+/*
+  Read more about the algorithms on wikipedia:
+  https://en.wikipedia.org/wiki/Rose_(mathematics)
+
+  Based on Coding Challenge #55 on the Coding Train
+  https://www.youtube.com/watch?v=f5QBExMNB1I&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=72
+*/
+
+// We need some variables to keep out values
+let d = 8;
+let n = 5;
+// We also need to make some sliders
+
+setup = () => {
+  createCanvas(400, 400);
+};
+
+draw = () => {
+  background(51);
+};
