5c555a55d2c35db1e3f502179bab399f70621ee0
Michael Houmann <mhoumann87@gmail.com>
2022-06-10 14:58:06 +0200

    Add peoject

--- 3_roses/sketch.js
+++ 3_roses/sketch.js
@@ -9,12 +9,58 @@
 // We need some variables to keep out values
 let d = 8;
 let n = 5;
-// We also need to make some sliders
+// We also need to make some sliders to control the values of n and d
+let sliderD, sliderN;
 
 setup = () => {
   createCanvas(400, 400);
+
+  // We "put" the sliders on the page
+  sliderD = createSlider(1, 8, 4);
+  sliderN = createSlider(1, 7, 5);
+  sliderD.input(draw);
+  sliderN.input(draw);
 };
 
 draw = () => {
   background(51);
+
+  // we get the values from the sliders and assign them to our variables
+  d = sliderD.value();
+  n = sliderN.value();
+
+  // We need a constant that are a value set by dividing n with d
+  const k = n / d;
+
+  // We start a new drawing state
+  push();
+
+  // We move 0,0 to the middle of the canvas from top left corner
+  translate(width * 0.5, height * 0.5);
+
+  // We start our rose drawing
+  beginShape();
+
+  stroke(184, 15, 10);
+  noFill();
+  strokeWeight(1);
+
+  // We make a loop based on TWO_PI (full circle), d and an arbitrary step
+  for (let a = 0; a < TWO_PI * d; a += 0.02) {
+    // Calculate the radius
+    const r = 200 * cos(k * a);
+    //get the x and y values based on r
+    const x = r * cos(a);
+    const y = r * sin(a);
+
+    vertex(x, y);
+  }
+
+  // We end our rose drawing, and make it a closed loop
+  endShape(CLOSE);
+
+  // we end the drawing state and restore original state
+  pop();
+  // We don't want this to keep on drawing
+  noLoop();
 };
